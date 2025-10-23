59b6eba26307d6d2934cb7c000350e5156e516f9
govardhan <ccgovardhan4@gmail.com>
2025-10-23 12:53:39 +0530

    u s

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home_view(request):
-    return HttpResponse("<h1>Hello DevOps! A fresh start.</h1>")
+    return HttpResponse("<h1>Hello DevOps! A fresh start yooo.</h1>")
